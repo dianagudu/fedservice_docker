f84dbc069f55a4e2a4d3145d64d22608ab4c001b
Diana Gudu <diana.gudu@kit.edu>
2024-12-16 23:54:36 +0100

    pin fedservice and idpy-oidc to last tested working version

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ cd deps || exit
 
 echo "################ Clone repositories"
 git clone https://github.com/rohe/fedservice
+git clone https://github.com/IdentityPython/idpy-oidc.git
 
 echo "################ Update repositories"
-cd fedservice && git pull && cd .. #git apply ../../fix_trust_marks.patch && cd ..
+cd fedservice && git pull && git checkout 8278eebd66a766cda4f11818d80decc608066f7c && cd ..
+cd idpy-oidc && git pull && git checkout issuer_metadata && git checkout 688c1b7a60a58853b3848d7ad0f7e4e5b6143c69 && cd ..
 
 echo "################ Build docker image"
 cd ..
